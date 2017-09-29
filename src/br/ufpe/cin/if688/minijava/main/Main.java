794078f0e25a6f8f79a724fa93d2c2dde29f52ba
Marcos Barreto <msb5@cin.ufpe.br>
2017-09-29 19:38:13 -0300

    Main modificado, agora utilizando o parser criado, e fazendo o teste com o PrettyPrintVisitor

--- src/br/ufpe/cin/if688/minijava/main/Main.java
+++ src/br/ufpe/cin/if688/minijava/main/Main.java
@@ -1,69 +1,28 @@
 package br.ufpe.cin.if688.minijava.main;
 
-import br.ufpe.cin.if688.minijava.ast.BooleanType;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclList;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
-import br.ufpe.cin.if688.minijava.ast.Identifier;
-import br.ufpe.cin.if688.minijava.ast.IdentifierType;
-import br.ufpe.cin.if688.minijava.ast.IntegerLiteral;
-import br.ufpe.cin.if688.minijava.ast.IntegerType;
-import br.ufpe.cin.if688.minijava.ast.MainClass;
-import br.ufpe.cin.if688.minijava.ast.MethodDeclList;
-import br.ufpe.cin.if688.minijava.ast.Print;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import br.ufpe.cin.if688.minijava.ast.Program;
-import br.ufpe.cin.if688.minijava.ast.VarDecl;
-import br.ufpe.cin.if688.minijava.ast.VarDeclList;
 import br.ufpe.cin.if688.minijava.visitor.PrettyPrintVisitor;
 
 public class Main {
 
-	public static void main(String[] args) {
-		MainClass main = new MainClass(
-				new Identifier("Teste"), 
-				new Identifier("Testando"), 
-				new Print(new IntegerLiteral(2))
-		);
-		
-		VarDeclList vdl1 = new VarDeclList();
-		vdl1.addElement(new VarDecl(
-			new BooleanType(),
-			new Identifier("flag")
-		));
-		vdl1.addElement(new VarDecl(
-				new IntegerType(),
-				new Identifier("num")
-		));
-		
-		MethodDeclList mdl = new MethodDeclList();
-		
-		ClassDeclSimple A = new ClassDeclSimple(
-					new Identifier("A"), vdl1, mdl
-		);
-		
-		ClassDeclExtends B = new ClassDeclExtends(
-				new Identifier("B"), new Identifier("A"), 
-				new VarDeclList(), new MethodDeclList()
-		);
-		
-		VarDeclList vdl2 = new VarDeclList();
-		vdl2.addElement(new VarDecl(
-				new IdentifierType("A"),
-				new Identifier("obj")
-		));
-		ClassDeclSimple C = new ClassDeclSimple(
-				new Identifier("C"), vdl2, new MethodDeclList()
-		);
-		
-		ClassDeclList cdl = new ClassDeclList();
-		cdl.addElement(A);
-		cdl.addElement(B);
-		cdl.addElement(C);
+	public static void main(String[] args) throws IOException {
+		InputStream stream = new FileInputStream("src/teste.txt");
+		ANTLRInputStream input = new ANTLRInputStream(stream);
+		msb5_las3Lexer lexer = new msb5_las3Lexer(input);
+		CommonTokenStream token = new CommonTokenStream(lexer);
+		msb5_las3Parser parser = new msb5_las3Parser(token);
+		ParseTree tree = parser.program();
 
-		Program p = new Program(main, cdl);
-		
-		PrettyPrintVisitor ppv = new PrettyPrintVisitor();
-		ppv.visit(p);
+		Visitor visitor = new Visitor();
+		Program prog = (Program) visitor.visit(tree);
+		prog.accept(new PrettyPrintVisitor());
 	}
-
 }
